02b66023012bd8b927e22b90b18308b5d6518feb
KKsblockchain <102418518+KKsblockchain@users.noreply.github.com>
2022-05-02 07:32:46 -0400

    Add files via upload

---
+++ kids_inheritance.sol
@@ -0,0 +1,99 @@
+//SPDX-License_identifier: UNLICENSED
+
+pragma solidity ^0.8.7;
+
+contract CryptoKids {
+    //owner Dad
+
+    address owner;
+
+    event logKidFundingReceived (address addr, uint amount, uint contractBalance);
+
+    constructor () {
+        owner = msg.sender;
+    }
+
+        //define Kid
+
+    struct Kid {
+        address payable walletAddress;
+        string firstName;
+        string lastName;
+        uint releaseTime;
+        uint amount;
+        bool canWithdraw;
+
+    }
+
+    Kid[] public kids;
+
+    modifier onlyOwner () {
+        require(msg.sender == owner, "only the owner can do this");
+        _;
+    }
+    //add kid to contract
+    function addKid(address payable walletAddress, string memory firstName, string memory lastName, uint releaseTime, uint amount, bool canWithdraw) public onlyOwner {
+        
+        kids.push( Kid( walletAddress,
+        firstName,
+        lastName,
+        releaseTime,
+        amount,
+        canWithdraw));
+
+    }
+
+    //get balance
+    function getBalance ()  public view returns (uint){
+        return address(this).balance;
+    }
+
+     //deposit funds to contract, to a specific kid's account
+    function deposit(address walletAddress) public payable {
+        addToKidsBalance(walletAddress);
+    }
+
+    function addToKidsBalance (address walletAddress) private {
+        for (uint i = 0; i < kids.length; i++){
+            if (kids[i].walletAddress == walletAddress){
+            kids[i].amount += msg.value;
+            emit logKidFundingReceived (walletAddress, msg.value, getBalance());
+            }
+        }
+    }
+   
+
+    function getIndex (address walletAddress) view private returns (uint) {
+        for (uint i = 0; i < kids.length; i++){
+            if (kids[i].walletAddress == walletAddress){
+            return i;
+            }
+        }
+            return 999;
+    }
+    //check if kid can withdraw
+
+    function availableToWithdraw(address walletAddress) public returns(bool) {
+        uint i = getIndex(walletAddress);
+        require(block.timestamp > kids[i].releaseTime, "it's not time yet");
+        if (block.timestamp > kids[i].releaseTime) {
+            kids[i].canWithdraw = true;
+        }
+        else{
+            kids[i].canWithdraw = false;
+        }
+        return kids[i].canWithdraw;
+
+    }
+
+    //withdraw money
+
+    function withdraw (address payable walletAddress) payable public {
+        
+        uint i = getIndex (walletAddress);
+        require(msg.sender == kids[i].walletAddress, "you must be the kid to withdraw");
+        require(kids[i].canWithdraw == true, "you can't withdraw yet");
+        kids[i].walletAddress.transfer(kids[i].amount);
+      
+    }
+}
